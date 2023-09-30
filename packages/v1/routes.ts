277d11ec3815d2c4b0d12d52e9b2ffa947ac7a5b
sujang958 <sujangdev@gmail.com>
2023-10-01 00:03:26 +0900

    feat: add a codegen

--- packages/v1/routes.ts
+++ packages/v1/routes.ts
@@ -2,18 +2,24 @@ import yoga from "@elysiajs/graphql-yoga"
 import Elysia from "elysia"
 import { parseCookie } from "./utils/http"
 import { join } from "path"
+import { Resolvers } from "./resolvers-types"
 
 const schemaFile = Bun.file(join(import.meta.dir, "./schema.graphql"))
 
 const typeDefs = await schemaFile.text()
 
-// graphql-scalars, graphql-codegen
+const resolvers: Resolvers = {
+  Query: { version: () => "v1-0.0.1" },
+}
+
+// graphql-scalars
 
 export const v1 = (app: Elysia<"/v1">) => {
   app.use(
     yoga({
       plugins: [],
       typeDefs,
+      resolvers,
       async context(initialContext) {
         const cookies = parseCookie(
           initialContext.request.headers.get("Cookie") ?? "",
@@ -23,7 +29,6 @@ export const v1 = (app: Elysia<"/v1">) => {
 
         return initialContext
       },
-      resolvers: {},
     }),
   )
 
